85a9f8ec3a3fe4c4a48bd6099f2a0d90babda363
Tyler Fuqua <tyler@agilityda.com>
2018-01-10 12:46:30 -0700

    Stretch Goal 2 Complete

--- HandsOn_Methods/src/com/methods/tyler/doubleMe.java
+++ HandsOn_Methods/src/com/methods/tyler/doubleMe.java
@@ -4,6 +4,7 @@ public class doubleMe {
 	// --- main method starts here ---
 	public static void main(String[] args) {
 		int num1 = 4;
+		int num2 = 2;
 	
 		
 		// invoke the "doubleMe2" method 
@@ -18,6 +19,13 @@ public class doubleMe {
 		System.out.println(doubleMe2(num1));
 		System.out.println(quadrupleMe(num1));
 		
+		// invoke the "doubleMe3" method
+		doubleMe3(num1, num2);
+		// Output
+		System.out.println(num1);
+		System.out.println(doubleMe3(num1, num2));
+		
+		
 	}
 	// --- doubleMe2 method starts here ---
 	private static int doubleMe2(int num1) {
@@ -28,6 +36,10 @@ public class doubleMe {
 	private static int quadrupleMe(int num1) {
 		// Return the value to the main method
 		return (doubleMe2(num1) * 2);
+	}
+	// --- doubleMe3 method starts here ---
+	private static int doubleMe3(int num1, int num2) {
+		return ((num1 * num2)*(num2)*(num2));
 	}
 
 }
