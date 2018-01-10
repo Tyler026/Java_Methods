4d5499206fd061f2db45e92ce82d5ef182f7a23a
Tyler Fuqua <tyler@agilityda.com>
2018-01-09 22:38:02 -0700

    Hands On Complete

---
+++ HandsOn_Methods/src/com/methods/tyler/doubleMe.java
@@ -0,0 +1,20 @@
+package com.methods.tyler;
+
+public class doubleMe {
+	// --- main method starts here ---
+	public static void main(String[] args) {
+		int num1 = 4;
+	
+		
+		// invoke the "doubleMe2" method with two integer parameters
+		doubleMe2(num1);
+		System.out.println(num1 * 2);
+
+	}
+
+	private static void doubleMe2(int num1) {
+		System.out.println(num1);
+		
+	}
+
+}
