77b4cf6e37769d4ee1b4485d4622f5a5efa5cb62
Tyler Fuqua <tyler@agilityda.com>
2018-01-10 12:32:36 -0700

    Fixed Errors

--- HandsOn_Methods/src/com/methods/tyler/doubleMe.java
+++ HandsOn_Methods/src/com/methods/tyler/doubleMe.java
@@ -9,23 +9,25 @@ public class doubleMe {
 		// invoke the "doubleMe2" method 
 		doubleMe2(num1);
 		// Output
-		System.out.println(num1 * 2);
+		System.out.println(num1);
+		System.out.println(doubleMe2(num1));
 		
 		// invoke the "quadrupleMe" method
 		quadrupleMe(num1);
 		// Output
-		System.out.println(num1 * 4);
+		System.out.println(doubleMe2(num1));
+		System.out.println(quadrupleMe(num1));
 		
 	}
 	// --- doubleMe2 method starts here ---
-	private static void doubleMe2(int num1) {
-		// Input
-		System.out.println(num1);
+	private static int doubleMe2(int num1) {
+		// Return the value to the main method
+		return (num1 * 2);
 	}
 	// --- quadrupleMe method starts here ---
-	private static void quadrupleMe(int num1) {
-		// Input
-		System.out.println(num1);
+	private static int quadrupleMe(int num1) {
+		// Return the value to the main method
+		return (doubleMe2(num1) * 2);
 	}
 
 }
